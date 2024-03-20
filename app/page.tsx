898ad032be070af4144241b24f3f06b8d5389b3b
anobes6416 <sebonaifa777@gmail.com>
2024-03-20 13:16:30 -0400

    client page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 import React, { FC, useState } from "react";
+import Heading from "./utils/Heading";
 
 
 interface Props { }
@@ -8,7 +9,11 @@ interface Props { }
 const Page: FC<Props>=(props) => {
   return (
     <div>
-
+      <Heading
+        title="Elearning"
+        description="Elearning is a platform for students to learn and get help from teachers"
+        keywords="programming, MERN, REDUX, cybersecurity"
+      />
     </div>
   )
 }
